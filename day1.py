d16416ae5fcb7563f442a53e449890b91bf08dcb
Jishnu Madhu <JishnuMadhu@users.noreply.github.com>
2026-08-06 21:42:51 +0530

    feat(day1): add fundamentals, conditionals, and logical operators

---
+++ day1.py
@@ -0,0 +1,122 @@
+"""print("Hi")
+a=20
+b = 40
+a,b = 30,40
+a = b = 10
+
+print(a)"""
+
+"""a = 10
+b = 20
+c = a
+a = b
+b = c
+print(a)
+print(b)
+"""
+"""a = 10
+b = 20
+a = a + b
+a = a - b
+b = a - b
+print(a)
+print(b)"""
+
+"""a= 10
+
+if a%2 == 0:
+    print("even")
+else:
+    print("odd")"""
+
+
+"""mark = 50
+if mark > 90:
+    prin("A")
+elif mark > 80:
+    print("B")
+elif mark > 70:
+    print(60)
+elif mark > 50:
+    print("D")
+elif mark > 40:
+    print("E")
+else:
+    print("Fail")"""
+
+
+#create a grade system
+#90-100 A
+#80-89 B
+#70-79 D
+#0-59 F
+
+"""mark = 50
+if mark > 90:
+    prin("A")
+elif mark > 80:
+    print("B")
+elif mark > 70:
+    print(60)
+elif mark > 50:
+    print("D")
+elif mark > 40:
+    print("E")
+else:
+    print("Fail")"""
+
+
+
+#check a number is positive or negative or zero
+ 
+"""num = 100
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")"""
+
+
+#check a number is even or odd
+
+
+"""num = 0
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")"""
+
+
+
+#check a number is divisible by 3 or not
+
+"""num = 99
+if num % 3 == 0:
+    print("Divisible by 3")
+else:
+    print("Not divisible by 3")"""
+
+#and operator
+
+""" true and true  = true
+    true and false = false
+    false and true = false
+    false and false = false"""
+
+#or operator
+
+""" true and true  = true
+    true and false = false
+    false and true = false
+    false and false = false"""
+
+#not operator
+
+"""not true = false
+   not false = true"""
+
+string1 = '0123456789'
+print(string1[-1:-10:-1])
+
+
