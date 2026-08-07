fa0556aa545d1ab1dedd6754aca133abd1d26098
Jishnu Madhu <JishnuMadhu@users.noreply.github.com>
2026-08-07 22:44:29 +0530

    feat(day7): add pattern printing exercises and string slicing

---
+++ day7.py
@@ -0,0 +1,106 @@
+# for i in range(4):
+#     for j in range(4):
+#         print("*",end = '')
+#     print()
+
+
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# for i in range(6):
+#     for j in range(i):
+#         print("*",end = '')
+#     print()
+
+
+
+
+# ******
+# *****
+# ****
+# ***
+# **
+# *
+
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print("*", end = '')
+#     print()
+
+
+# 1
+# 22
+# 333
+# 4444
+
+# for i in range(1,5):
+#     for j in range(i):        
+#         print(i,end = '')    
+#     print()
+
+
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# count = 1
+# for i in range(1,5):
+#     for j in range(1,i+1):        
+#         print(count,end = ' ') 
+#         count+=1   
+#     print()
+
+
+# string1 = 'APPLE'
+# A
+# A P
+# A P P
+# A P P L 
+# A P P L E
+
+string1 = 'APPLE'
+# for i in range(len(string1)):
+#     for j in range(i+1):        
+#         print(string1[j],end = '')    
+#     print()
+
+# with one for loop
+for i in range(len(string1)):
+    print(string1[0:i+1])
+
+************************
+submit monday
+
+# *****
+# *   *
+# *   *
+
+
+
+
+     *
+    **
+    ***
+
+
+
+******************************************
+
+WBWBWB
+BWBWBW
+WBWBWB
+BWBWBW
+
+
+1010101
+0101010
+1010101
+0101010
+1010101
+
+
+*#*
